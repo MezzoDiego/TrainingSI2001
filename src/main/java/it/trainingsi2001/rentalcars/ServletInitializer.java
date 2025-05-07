6a7f949266534859d3ab02fbcb24a6553739fd9b
Diego Mezzo <diegomezzo90@gmail.com>
2025-05-07 09:21:55 +0200

    primo commit

---
+++ src/main/java/it/trainingsi2001/rentalcars/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.trainingsi2001.rentalcars;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RentalCarsApplication.class);
+	}
+
+}
---
+++ src/test/java/it/trainingsi2001/rentalcars/RentalcarsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.trainingsi2001.rentalcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalcarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
